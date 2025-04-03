aefd166cea373451e55a3987dc8c610e30fcf737
ano0p5 <an00pkumar.ake@gmail.com>
2025-04-03 16:22:19 +0530

    json_validator

--- 2025-4-03/csv_json/json/json_line_array.py
+++ 2025-4-03/csv_json/json/json_line_array.py
@@ -15,6 +15,6 @@ def json_line_format_to_array(input_jsonl, output_json):
     with open(output_json, mode='w', encoding='utf-8') as file:
         json.dump(data, file, indent=4, ensure_ascii=False)
 
-# Example usage
-json_array_to_line_format('/home/anoop/frt/2025-4-03/csv_json/output.json', '/home/anoop/frt/2025-4-03/csv_json/output_line.jsonl')
-json_line_format_to_array('/home/anoop/frt/2025-4-03/csv_json/output_line.jsonl', '/home/anoop/frt/2025-4-03/csv_json/output_array.json')
+
+json_array_to_line_format('/home/anoop/frt/2025-4-03/csv_json/json/products.json', '/home/anoop/frt/2025-4-03/csv_json/json/output_line.jsonl')
+json_line_format_to_array('/home/anoop/frt/2025-4-03/csv_json/json/output_line.jsonl', '/home/anoop/frt/2025-4-03/csv_json/json/output_array.json')
